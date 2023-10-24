c66b2ad4dfac965721d96e7bd76d1fc61e58c875
EmanAbobakr <43084491+EmanAbobakr@users.noreply.github.com>
2023-10-24 19:01:27 +0300

    add start game screen logic

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
+import React from "react";
 import { StyleSheet, ImageBackground } from "react-native";
 import { StartGameScreen } from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
+import { GameScreen } from "./screens/GameScreen";
 
 export default function App() {
+  const [enteredNum, setEnteredNum] = React.useState();
+
+  function pickedNumHandler(pickedNum) {
+    setEnteredNum(pickedNum);
+  }
+
+  let screen = <StartGameScreen onConfirmNum={pickedNumHandler} />;
+
+  if (enteredNum) {
+    screen = <GameScreen />;
+  }
   return (
     <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
       <ImageBackground
@@ -11,7 +24,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backGroundImage}
       >
-        <StartGameScreen />
+        {screen}
       </ImageBackground>
     </LinearGradient>
   );
