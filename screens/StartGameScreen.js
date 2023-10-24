30c282e736759724cffb2c3292e94c234e88590e
EmanAbobakr <43084491+EmanAbobakr@users.noreply.github.com>
2023-10-24 20:57:17 +0300

    update PrimaryButton Component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View, TextInput, Alert } from "react-native";
-import { PrimaryButton } from "../components/PrimaryButton";
+import { PrimaryButton } from "../components/ui/PrimaryButton";
 import { Colors } from "../constants/colors";
 
 export function StartGameScreen({ onConfirmNum }) {
